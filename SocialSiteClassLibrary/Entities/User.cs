0dc1a0f35c31014c82d7041d9d39f5b940ed8e6d
Mirzo Dadokhonov <mdm_01@bk.ru>
2024-04-04 08:11:01 +0500

    PhoneNumber added to User

--- SocialSiteClassLibrary/Entities/User.cs
+++ SocialSiteClassLibrary/Entities/User.cs
@@ -8,6 +8,7 @@
         public string Patronymic { get; set; }
         public string Email { get; set; }
         public string Password { get; set; }
+        public string PhoneNumber { get; set; }
 
     }
 }
