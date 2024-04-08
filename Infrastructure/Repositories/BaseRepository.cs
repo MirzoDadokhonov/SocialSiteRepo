7649ef6ceb1947d183d416073e98fbc3e431f3f6
Mirzo Dadokhonov <mdm_01@bk.ru>
2024-04-08 08:47:52 +0500

    Added Base Repository and Application and Infrastructure projects

---
+++ Infrastructure/Repositories/BaseRepository.cs
@@ -0,0 +1,32 @@
+﻿using SocialSiteClassLibrary.Interfaces.Repositories;
+
+namespace Infrastructure.Repositories
+{
+    public class BaseRepository<T> : IBaseRepository<T> where T : class
+    {
+        public bool Delete(T element)
+        {
+            throw new NotImplementedException();
+        }
+
+        public T Get(Guid Id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<T> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public T Insert(T element)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Update(T element)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ SocialSiteClassLibrary/Interfaces/Repositories/IBaseRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SocialSiteClassLibrary.Interfaces.Repositories
+{
+    public interface IBaseRepository<T> where T : class
+    {
+        T Insert(T element);
+        bool Update(T element);
+        bool Delete(T element);
+        T Get(Guid Id);
+        IEnumerable<T> GetAll();
+    }
+}
